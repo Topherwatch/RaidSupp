bc09771a47c7d2a6a2e182c564a726a0b9667668
Topherwatch <40777845+Topherwatch@users.noreply.github.com>
2018-07-06 16:14:07 -0400

    Add files via upload

---
+++ src/raidG/classes/abilities/Tranquility.java
@@ -0,0 +1,49 @@
+package raidG.classes.abilities;
+
+public class Tranquility extends abilities{
+	
+	//time in seconds until usable.
+	int cooldown=180;
+	
+	//time in seconds when ready to be used(starts at 0);
+	int readyToUse=0;
+	
+	//returns if ability is ready to be used or not.
+	boolean ussable=true;
+	
+	//Id of skill for ERT note
+	int skillId=740;
+	
+	public void use(int time) {
+
+		readyToUse=time+cooldown;
+		System.out.println("Tranquility used at: "+time);
+		
+	}
+	
+	public int getCooldown() {
+		return this.cooldown;
+	}
+	
+	public void setCooldown(int cooldown) {
+		this.cooldown=cooldown;
+	
+	}
+	
+	public boolean isUsable(int time) {
+		if(time>=readyToUse) {
+			this.ussable=true;
+		}
+		else if (time<readyToUse) {
+			System.out.println("not useable until:"+readyToUse);
+			this.ussable=false;
+		}
+		return this.ussable;
+	}
+	
+	public void setUssibility(boolean variable) {
+		this.readyToUse=0;
+		this.ussable=variable;
+	}
+
+}
